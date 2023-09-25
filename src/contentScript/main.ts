9b650154b8e4bce85543ad1506d7c4d477fa4e7b
Cside <cside.story@gmail.com>
2023-09-25 13:41:34 +0900

    markdown mode only

--- src/contentScript/main.ts
+++ src/contentScript/main.ts
@@ -1,7 +1,9 @@
-const input = document.querySelector<HTMLInputElement>('#body');
-if (!input) throw new Error('input#body is not found');
+if (document.querySelector<HTMLInputElement>('input#syntax')?.value === 'markdown') {
+  const input = document.querySelector<HTMLInputElement>('#body');
+  if (!input) throw new Error('input#body is not found');
 
-input.addEventListener('keypress', (event) => {
-  const ret = MarkdownHelper(input, event);
-  if (ret === false) return event.preventDefault();
-});
+  input.addEventListener('keypress', (event) => {
+    const ret = MarkdownHelper(input, event);
+    if (ret === false) return event.preventDefault();
+  });
+}
